2727f52d9a2683b84f33a5f7a6ba01f647c2a30a
Eluf <elifyuce99@gmail.com>
2025-03-11 15:16:26 +0300

    composition project done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,4 @@
 package org.example;
-
-import org.example.model.Bed;
-import org.example.model.Bedroom;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
